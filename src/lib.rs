3ce6e55be68c3e5fb4331f10e3c49e48c3b3fac5
Mara Schulke <mara.schulke@helsing.ai>
2024-03-27 08:44:33 +0000

    Update the lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub use hackathon_macros::main;
+pub use hs_hackathon_macros::main;
 
 /// The prelude module to jumpstart your implementation
 pub mod prelude {
@@ -15,18 +15,18 @@ pub mod prelude {
 
 /// A library for controling the drone, as well as recieving frames from its camera
 pub mod drone {
-    pub use hackathon_drone::*;
+    pub use hs_hackathon_drone::*;
 }
 
 /// A hardware abstraction layer over the motor and wheels of the RC car
 pub mod car {
-    pub use hackathon_car::{Angle, MotorSocket, Velocity, WheelOrientation};
+    pub use hs_hackathon_car::{Angle, MotorSocket, Velocity, WheelOrientation};
 }
 
 /// A computer vision api to detect LEDs inside of video frames recieved from drones
 pub mod vision {
-    pub use hackathon_vision::{detect, distance};
-    pub use hackathon_vision::{Color, Led, LedDetectionConfig};
+    pub use hs_hackathon_vision::{detect, distance};
+    pub use hs_hackathon_vision::{Color, Led, LedDetectionConfig};
 }
 
 /// Cheats that you may use if you get stuck on a particular implementation detail
